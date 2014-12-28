fe46bf6cd635f4eacc42eeddf113fa803f5d9253
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-12-28 18:08:47 +0200

    Added fields and add method .

--- Ch6/src/Rational.scala
+++ Ch6/src/Rational.scala
@@ -3,6 +3,14 @@
  */
 class Rational(n: Int, d: Int) {
     require (d != 0)
-    override def toString = n + "/" + d
+    val numer = n
+    val denom = d
+    override def toString = numer + "/" + denom
+
+    def add(that: Rational): Rational =
+        new Rational(
+            numer * that.denom + that.numer * denom,
+            denom * that.denom
+        )
 }
 

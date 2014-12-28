9eb169391eedec6f7f6cb8bd3b74d188bb96a9fe
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-12-28 19:01:33 +0200

    Added lessThan and max.

--- Ch6/src/Rational.scala
+++ Ch6/src/Rational.scala
@@ -12,5 +12,11 @@ class Rational(n: Int, d: Int) {
             numer * that.denom + that.numer * denom,
             denom * that.denom
         )
+
+    def lessThan(that: Rational) =
+        this.numer * that.denom < that.numer * this.denom
+
+    def max(that: Rational) =
+        if (lessThan(that)) that else this
 }
 

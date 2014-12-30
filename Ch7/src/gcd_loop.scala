409966dc853ec6f46d8269501eaaf22e173d9e02
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2014-12-30 22:41:18 +0200

    added gcd with loop

---
+++ Ch7/src/gcd_loop.scala
@@ -0,0 +1,15 @@
+/**
+ * Created by sukruhasdemir on 30/12/14.
+ */
+
+def gcdLoop(x: Long, y: Long): Long = {
+    var a = x
+    var b = y
+
+    while (a != 0) {
+        val temp = a
+        a = b % a
+        b = temp
+    }
+    b
+}

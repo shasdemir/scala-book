167fb3a541925bd2894aae791c3aa727c26a2b73
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-03 15:17:22 +0200

    merged ArrayElement and Element in the same file.

--- src/main/scala/Ch10/Element.scala
+++ src/main/scala/Ch10/Element.scala
@@ -5,3 +5,17 @@ abstract class Element {
     def height: Int = contents.length
     def width: Int = if (height == 0) 0 else contents(0).length
 }
+
+class ArrayElement(conts: Array[String]) extends Element {
+
+    require {  // every string element must be the same size
+        val lengthSet = Set(
+            for (line <- conts)
+            yield line.length
+        )
+
+        lengthSet.size == 1
+    }
+
+    def contents = conts
+}3

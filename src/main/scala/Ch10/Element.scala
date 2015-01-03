ad9da931ddd87e883db50e0a3ae2d1c714c01add
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-03 22:28:27 +0200

    better require definition

--- src/main/scala/Ch10/Element.scala
+++ src/main/scala/Ch10/Element.scala
@@ -27,12 +27,17 @@ object Element {
     private class ArrayElement(val contents: Array[String]) extends Element {
 
         require {  // every string element must be the same size
-        val lengthSet = Set(
-                for (line <- contents)
-                yield line.length
-            )
+            val lengthSet = Set(
+                    for (line <- contents)
+                    yield line.length
+                )
 
             lengthSet.size == 1
+        }
+
+        require {
+            val headLength = contents.head.length
+            contents.toList.forall(_.length == headLength)
         }
     }
 

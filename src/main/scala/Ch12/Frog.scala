49f4072812c845c2d7c27f2d8749031b204acb98
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-06 01:49:53 +0200

    Added Frog class and Philosophical trait.

---
+++ src/main/scala/Ch12/Frog.scala
@@ -0,0 +1,8 @@
+package main.scala.Ch12
+
+/**
+ * Created by sukruhasdemir on 06/01/15.
+ */
+class Frog extends Philosophical {
+    override def toString = "green"
+}
---
+++ src/main/scala/Ch12/Philosophical.scala
@@ -0,0 +1,8 @@
+package main.scala.Ch12
+
+
+trait Philosophical {
+    def philosophize(): Unit = {
+        println("I consume memory, therefore I am!")
+    }
+}

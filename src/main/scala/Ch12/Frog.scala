e9ffd356e6a30571b91c24f2f382a525c64e8de0
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-06 02:01:06 +0200

    Frog override

--- src/main/scala/Ch12/Frog.scala
+++ src/main/scala/Ch12/Frog.scala
@@ -5,4 +5,7 @@ package main.scala.Ch12
  */
 class Frog extends Philosophical {
     override def toString = "green"
+    override def philosophize(): Unit = {
+        println("It ain't easy being " + toString + "!")
+    }
 }

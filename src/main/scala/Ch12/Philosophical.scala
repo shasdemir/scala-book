3ec5a4ccda670e925263bcc2326b935c0e340e44
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-07 20:31:23 +0200

    Added Rectangle class. Small typo fix on Philosophical.

--- src/main/scala/Ch12/Philosophical.scala
+++ src/main/scala/Ch12/Philosophical.scala
@@ -5,4 +5,4 @@ trait Philosophical {
     def philosophize(): Unit = {
         println("I consume memory, therefore I am!")
     }
-}`
+}
---
+++ src/main/scala/Ch12/Rectangle.scala
@@ -0,0 +1,8 @@
+package Ch12
+
+/**
+ * Created by sukruhasdemir on 07/01/15.
+ */
+class Rectangle(val topLeft: Point, val bottomRight: Point) extends Rectangular {
+    // bla bla
+}

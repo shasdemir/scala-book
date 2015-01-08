000fe60932b162312c1b581dab5bc7945ccf8e67
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-08 02:00:08 +0200

    Added Incrementing and Filtering.

--- src/main/scala/Ch12/IntQueue.scala
+++ src/main/scala/Ch12/IntQueue.scala
@@ -20,3 +20,16 @@ class BasicIntQueue extends IntQueue {
 trait Doubling extends IntQueue {
     abstract override def put(x: Int) { super.put(2 * x) }
 }
+
+
+trait Incrementing extends IntQueue {
+    abstract override def put(x: Int) { super.put(x + 1) }
+}
+
+
+trait Filtering extends IntQueue {
+    abstract override def put(x: Int): Unit = {
+        if (x >= 0)
+            super.put(x)
+    }
+}

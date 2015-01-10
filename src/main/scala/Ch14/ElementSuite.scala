3eaecc0cdf67518b0cbcaad41a8b2096a747995a
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-10 20:50:56 +0200

    Added intercept.

--- src/main/scala/Ch14/ElementSuite.scala
+++ src/main/scala/Ch14/ElementSuite.scala
@@ -10,8 +10,12 @@ class ElementSuite extends FunSuite {
 
     test("elem result should have the same width as passed.") {  // name of the test
         val ele = elem('x', 2, 3)
-        assertResult(3) {
+        assertResult(2) {
             ele.width
         }
+    }
+
+    intercept[IllegalArgumentException] {
+        elem('x', -2, 3)  // fails
     }
 }

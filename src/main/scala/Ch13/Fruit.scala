a87e3b0f73a1d745c7eef2fed61cdca5a6ab5e34
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-10 02:22:07 +0200

    Added Fruits.

---
+++ src/main/scala/Ch13/Fruit.scala
@@ -0,0 +1,17 @@
+package Ch13
+
+/**
+ * Created by sukruhasdemir on 10/01/15.
+ */
+abstract class Fruit (
+    val name: String,
+    val color: String
+)
+
+
+object Fruits {
+    object Apple extends Fruit("apple", "red")
+    object Orange extends Fruit("orange", "orange")
+    object Pear extends Fruit("pear", "yellowish")
+    val menu = List(Apple, Orange, Pear)
+}

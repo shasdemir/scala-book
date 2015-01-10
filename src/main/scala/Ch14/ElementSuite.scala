43e94dfea9b480a0467a942f169ae63e1fc52a07
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-10 19:58:52 +0200

    Test using FunSuite.

--- src/main/scala/Ch14/ElementSuite.scala
+++ src/main/scala/Ch14/ElementSuite.scala
@@ -1,13 +1,14 @@
 package Ch14
 
-import org.scalatest.Suite
+import org.scalatest.FunSuite
 import Ch10.Element.elem
 
 /**
  * Created by sukruhasdemir on 10/01/15.
  */
-class ElementSuite extends Suite {
-    def testUniformElement(): Unit = {
+class ElementSuite extends FunSuite {
+
+    test("elem result should have the same passed width") {  // name of the test
         val ele = elem('x', 2, 3)
         assert(ele.width == 2)
     }

e56897136cd304ad5c387df092bef01f80c3e1b2
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-12 01:13:44 +0200

    ScalaCheck doesn't stop.

--- src/test/scala/Ch14/ElementSpec2.scala
+++ src/test/scala/Ch14/ElementSpec2.scala
@@ -10,14 +10,14 @@ import org.scalacheck.Prop._
  */
 class ElementSpec2 extends WordSpec with Checkers {
 
-//    "elem result" must {
-//        "have passed width" in {
-//            check((w: Int) => (w > 0) ==> (elem('x', w, 3).width == w))
-//        }
-//        "have passed height" in {
-//            check((h: Int) => (h > 0) ==> (elem('x', 2 ,h).height == h))
-//        }
-//    }
+    "elem result" must {
+        "have passed width" in {
+            check((w: Int) => (w > 0) ==> (elem('x', w, 3).width == w))
+        }
+        "have passed height" in {
+            check((h: Int) => (h > 0) ==> (elem('x', 2 ,h).height == h))
+        }
+    }
 
 //    val propWidth = forAll { w: Int =>
 //        (w > 0) ==> (elem('x', w, 3).width == w) }

661b49217af298e30f4a6025c8d0f829ee74a2b2
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-12 01:29:05 +0200

    Added Expr.

---
+++ src/main/scala/Ch15/Expr.scala
@@ -0,0 +1,10 @@
+package Ch15
+
+/**
+ * Created by sukruhasdemir on 12/01/15.
+ */
+abstract class Expr
+case class Var(name: String) extends Expr
+case class Number(num: Double) extends Expr
+case class UnOp(operator: String, arg: Expr) extends Expr
+case class BinOp(operator: String, leftArg: Expr, rightArg: Expr) extends Expr

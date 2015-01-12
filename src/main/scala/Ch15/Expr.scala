cd4969ed7965bf5991396de7b4577e2924ea2f36
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-12 02:52:33 +0200

    Added tupleDemo.

--- src/main/scala/Ch15/Expr.scala
+++ src/main/scala/Ch15/Expr.scala
@@ -20,12 +20,17 @@ object Simplifier {
 }
 
 
-object Describer {
+object Demos {
     def describe(x: Any):String = x match {
         case 5 => "five"
         case true => "truth"
         case "hello" => "hi!"
         case Nil => "the empty list"
         case _ => "something else"
+    }
+
+    def tupleDemo(expr: Any) = expr match {
+        case (a, b, c) => println("matched " + a + b + c)
+        case _ =>
     }
 }

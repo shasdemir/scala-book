c37281c2d2854315a1ffa1150c4e178980fc82e0
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-12 02:12:37 +0200

    Added simplifyTop.

--- src/main/scala/Ch15/Expr.scala
+++ src/main/scala/Ch15/Expr.scala
@@ -8,3 +8,13 @@ case class Var(name: String) extends Expr
 case class Number(num: Double) extends Expr
 case class UnOp(operator: String, arg: Expr) extends Expr
 case class BinOp(operator: String, leftArg: Expr, rightArg: Expr) extends Expr
+
+
+object Simplifier {
+    def simplifyTop(expr: Expr): Expr = expr match {
+        case UnOp("-", UnOp("-", e)) => e  // Double negative
+        case BinOp("+", e, Number(0)) => e  // Adding zero
+        case BinOp("*", e, Number(1)) => e  // Multiplying by one
+        case _ => expr
+    }
+}

19f6509bbc6c8c5f7185ea8e2c9a107809d40589
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-12 16:41:58 +0200

    Match functions are now in a different file.

--- src/main/scala/Ch15/Expr.scala
+++ src/main/scala/Ch15/Expr.scala
@@ -18,25 +18,3 @@ object Simplifier {
         case _ => expr
     }
 }
-
-
-object Demos {
-    def describe(x: Any):String = x match {
-        case 5 => "five"
-        case true => "truth"
-        case "hello" => "hi!"
-        case Nil => "the empty list"
-        case _ => "something else"
-    }
-
-    def tupleDemo(expr: Any) = expr match {
-        case (a, b, c) => println("matched " + a + b + c)
-        case _ =>
-    }
-
-    def generalSize(x: Any) = x match {
-        case s: String => s.length
-        case m: Map[_, _] => m.size
-        case _ => -1
-    }
-}
---
+++ src/main/scala/Ch15/MatchDemos.scala
@@ -0,0 +1,30 @@
+package Ch15
+
+/**
+ * Created by sukruhasdemir on 12/01/15.
+ */
+object MatchDemos {
+    def describe(x: Any):String = x match {
+        case 5 => "five"
+        case true => "truth"
+        case "hello" => "hi!"
+        case Nil => "the empty list"
+        case _ => "something else"
+    }
+
+    def tupleDemo(expr: Any) = expr match {
+        case (a, b, c) => println("matched " + a + b + c)
+        case _ =>
+    }
+
+    def generalSize(x: Any) = x match {
+        case s: String => s.length
+        case m: Map[_, _] => m.size
+        case _ => -1
+    }
+
+    def isIntIntMap(x: Any) = x match {
+        case m: Map[Int, Int] => true
+        case _ => false
+    }
+}

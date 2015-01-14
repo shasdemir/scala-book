64140fe0b12a5bf2fecd8553bb3adf85ffa2a18f
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-14 21:00:01 +0200

    Rearranged insertionSort.

--- src/main/scala/Ch16/ListOps.scala
+++ src/main/scala/Ch16/ListOps.scala
@@ -4,13 +4,14 @@ package Ch16
  * Created by sukruhasdemir on 14/01/15.
  */
 object Sorting {
-    def insertionSort(xList: List[Int]): List[Int] =
+    def insertionSort(xList: List[Int]): List[Int] = {
+        def insert(x: Int, xList: List[Int]): List[Int] =
+            if (xList.isEmpty || x <= xList.head) x :: xList
+            else xList.head :: insert(x, xList.tail)
+
         if (xList.isEmpty) Nil
         else insert(xList.head, insertionSort(xList.tail))
-
-    private def insert(x: Int, xList: List[Int]): List[Int] =
-        if (xList.isEmpty || x <= xList.head) x :: xList
-        else xList.head :: insert(x, xList.tail)
+        }
 }
 
 object ListOps {

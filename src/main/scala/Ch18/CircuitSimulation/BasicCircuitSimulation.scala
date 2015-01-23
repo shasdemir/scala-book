65bf7873946edd074c790e4fd21271db29b9fddb
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-24 00:03:39 +0200

    Added andGate.

--- src/main/scala/Ch18/CircuitSimulation/BasicCircuitSimulation.scala
+++ src/main/scala/Ch18/CircuitSimulation/BasicCircuitSimulation.scala
@@ -37,4 +37,16 @@ abstract class BasicCircuitSimulation extends Simulation {
         }
         input addAction invertAction
     }
+
+    def andGate(a1: Wire, a2: Wire, output: Wire) = {
+        def andAction() = {
+            val a1Sig = a1.getSignal
+            val a2Sig = a2.getSignal
+            afterDelay(AndGateDelay) {
+                output setSignal (a1Sig & a2Sig)
+            }
+        }
+        a1 addAction andAction
+        a2 addAction andAction
+    }
 }

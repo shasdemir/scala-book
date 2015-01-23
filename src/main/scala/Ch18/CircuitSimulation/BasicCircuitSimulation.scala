fb1fb8fa7cb33628e618312c0ef4ee477177e16b
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-24 00:06:13 +0200

    Added orGate.

--- src/main/scala/Ch18/CircuitSimulation/BasicCircuitSimulation.scala
+++ src/main/scala/Ch18/CircuitSimulation/BasicCircuitSimulation.scala
@@ -49,4 +49,16 @@ abstract class BasicCircuitSimulation extends Simulation {
         a1 addAction andAction
         a2 addAction andAction
     }
+
+    def orGate(a1: Wire, a2: Wire, output: Wire) = {
+        def orAction() = {
+            val a1Sig = a1.getSignal
+            val a2Sig = a2.getSignal
+            afterDelay(OrGateDelay) {
+                output setSignal (a1Sig & a2Sig)
+            }
+        }
+        a1 addAction orAction
+        a2 addAction orAction
+    }
 }

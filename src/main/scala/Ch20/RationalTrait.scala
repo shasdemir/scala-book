bf2a8ad2074d267fd46c734666d630b5460621b2
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-28 20:48:48 +0200

    Added RationalClass.

--- src/main/scala/Ch20/RationalTrait.scala
+++ src/main/scala/Ch20/RationalTrait.scala
@@ -25,3 +25,14 @@ object TwoThirds extends {
     val numerArg = 2
     val denomArg = 3
 } with RationalTrait
+
+
+class RationalClass(n: Int, d: Int) extends {
+    val numerArg = n
+    val denomArg = d
+} with RationalTrait {
+    def + (that: RationalClass) = new RationalClass(
+        numer * that.denom + that.numer * denom,
+        denom * that.denom
+    )
+}

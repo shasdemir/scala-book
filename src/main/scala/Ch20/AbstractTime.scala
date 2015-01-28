6865425cda0f3895e33c1d929cc386c678a1812a
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-28 16:32:51 +0200

    Added RationalTrait

---
+++ src/main/scala/Ch20/AbstractTime.scala
@@ -0,0 +1,9 @@
+package Ch20
+
+/**
+ * Created by sukruhasdemir on 28/01/15.
+ */
+trait AbstractTime {
+    var hour: Int
+    var minute: Int
+}
---
+++ src/main/scala/Ch20/RationalTrait.scala
@@ -0,0 +1,9 @@
+package Ch20
+
+/**
+ * Created by sukruhasdemir on 28/01/15.
+ */
+trait RationalTrait {
+    val numerArg: Int
+    val denomArg: Int
+}

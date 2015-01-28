fdade57549f6354ee3cdc0f6db284559b6ac3307
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-28 15:52:23 +0200

    Added Concrete.

--- src/main/scala/Ch20/Abstract.scala
+++ src/main/scala/Ch20/Abstract.scala
@@ -9,3 +9,11 @@ trait Abstract {
     val initial: T
     var current: T
 }
+
+
+class Concrete extends Abstract {
+    type T = String
+    def transform(x: String) = x + x
+    val initial = "hi"
+    var current = initial
+}

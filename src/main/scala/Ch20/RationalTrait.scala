dd7431a36648240167d32b133e65e456105ab58b
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-28 20:22:21 +0200

    More RationalTrait...

--- src/main/scala/Ch20/RationalTrait.scala
+++ src/main/scala/Ch20/RationalTrait.scala
@@ -6,4 +6,16 @@ package Ch20
 trait RationalTrait {
     val numerArg: Int
     val denomArg: Int
+
+    require(denomArg != 0)
+
+    private val g = gcd(numerArg, denomArg)
+
+    val numer = numerArg / g
+    val denom = denomArg / g
+
+    private def gcd(a: Int, b: Int): Int =
+        if (b == 0) a else gcd(b, a % b)
+
+    override def toString = numer + "/" + denom
 }

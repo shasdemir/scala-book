7e96e76920905adaca5a059bb0ad4caa72f444f3
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-28 21:31:33 +0200

    Added LazyRationalTrait.

--- src/main/scala/Ch20/RationalTrait.scala
+++ src/main/scala/Ch20/RationalTrait.scala
@@ -36,3 +36,22 @@ class RationalClass(n: Int, d: Int) extends {
         denom * that.denom
     )
 }
+
+
+trait LazyRationalTrait {
+    val numerArg: Int
+    val denomArg: Int
+
+    lazy val numer = numerArg / g
+    lazy val denom = denomArg / g
+
+    private lazy val g = {
+        require(denomArg != 0)
+        gcd(numerArg, denomArg)
+    }
+
+    private def gcd(a: Int, b: Int): Int =
+        if (b == 0) a else gcd(b, a % b)
+
+    override def toString = numer + "/" + denom
+}

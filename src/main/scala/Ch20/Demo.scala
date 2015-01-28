1b068958b7fa35a1606aab1c6801c643ff9c2d5a
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-28 21:25:06 +0200

    Demo.x is now a lazy val.

--- src/main/scala/Ch20/Demo.scala
+++ src/main/scala/Ch20/Demo.scala
@@ -4,5 +4,5 @@ package Ch20
  * Created by sukruhasdemir on 28/01/15.
  */
 object Demo {
-    val x = { println("initializing x"); "done" }
+    lazy val x = { println("initializing x"); "done" }
 }

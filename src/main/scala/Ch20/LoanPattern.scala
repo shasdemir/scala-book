fa3704534eb92f76f6ddbf67919337b534f19a1e
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-30 21:38:43 +0200

    Added using, implementing Loan Pattern.

---
+++ src/main/scala/Ch20/LoanPattern.scala
@@ -0,0 +1,12 @@
+package Ch20
+
+/**
+ * Created by sukruhasdemir on 30/01/15.
+ */
+object LoanPattern {
+    def using[T <: { def close(): Unit }, S](obj: T)(operation: T => S) = {
+        val result = operation(obj)
+        obj.close()
+        result
+    }
+}

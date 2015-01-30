bb118c5c4fe6ec6165877a7438ec7d879a05b6f1
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-30 22:07:42 +0200

    Added Color enumeration.

---
+++ src/main/scala/Ch20/Color.scala
@@ -0,0 +1,8 @@
+package Ch20
+
+/**
+ * Created by sukruhasdemir on 30/01/15.
+ */
+object Color extends Enumeration {
+    val Red, Green, Blue = Value
+}
--- src/main/scala/Ch20/LoanPattern.scala
+++ src/main/scala/Ch20/LoanPattern.scala
@@ -3,6 +3,8 @@ package Ch20
 /**
  * Created by sukruhasdemir on 30/01/15.
  */
+
+// no base type is specified, so AnyRef is used
 object LoanPattern {
     def using[T <: { def close(): Unit }, S](obj: T)(operation: T => S) = {
         val result = operation(obj)

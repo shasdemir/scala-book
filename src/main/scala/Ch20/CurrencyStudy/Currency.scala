d2f49849658277e9b67381ff622eacefb853a80f
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-31 03:19:51 +0200

    Also added my non-compiling attempt.

--- src/main/scala/Ch20/CurrencyStudy/Currency.scala
+++ src/main/scala/Ch20/CurrencyStudy/Currency.scala
@@ -12,3 +12,38 @@ abstract class Currency {  // faulty design
     def + (that: Currency): Currency
     def * (x: Double): Currency
 }
+
+
+
+
+class CurrencyDesignation
+class EuroDesignation extends CurrencyDesignation
+class USDDesignation extends CurrencyDesignation
+
+abstract class SCurrency {
+    val amount: Long
+    def designation: String
+
+    override def toString = amount+ " " + designation
+
+    def + (that: SCurrency): SCurrency
+    def * (x: Double): SCurrency
+}
+
+
+class EuroAmount(val amount: Long) extends SCurrency {
+    val designation = "Euro"
+
+    def + (that: EuroAmount): EuroAmount = new EuroAmount(amount + that.amount)
+
+    def * (x: Double): EuroAmount = new EuroAmount((amount * x).toLong)
+}
+
+
+class USDAmount(val amount: Long) extends SCurrency {
+    val designation = "USD"
+
+    def + (that: USDAmount): USDAmount = new USDAmount(amount + that.amount)
+
+    def * (x: Double): USDAmount = new USDAmount((amount * x).toLong)
+}

1ad133c03c6b94b577342a85bd2c7b07d9025514
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-02-01 01:09:48 +0200

    Added Europe and Japan.

--- src/main/scala/Ch20/CurrencyStudy/Currency.scala
+++ src/main/scala/Ch20/CurrencyStudy/Currency.scala
@@ -39,6 +39,31 @@ object US extends CurrencyZone {  // all this is just to prevent currency amount
 }
 
 
+object Europe extends CurrencyZone {
+    abstract class Euro extends AbstractCurrency {
+        def designation = "EUR"
+    }
+
+    type Currency = Euro
+    def make(cents: Long) = new Euro { val amount = cents }
+
+    val Cent = make(1)
+    val Euro = make(100)
+    val currencyUnit = Euro
+}
+
+
+object Japan extends CurrencyZone {
+    abstract class Yen extends AbstractCurrency {
+        def designation = "JPY"
+    }
+
+    type Currency = Yen
+    def make(yen: Long) = new Yen { val amount = yen }
+
+    val Yen = make(1)
+    val CurrencyUnit = Yen
+}
 
 
 // -------------------------------------------------------- below is my own attempt at this, above is from the book

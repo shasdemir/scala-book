777f84f603b831a2a2dab11de091afb970ed0eb8
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-02-01 01:20:19 +0200

    Added the currency conversion table.

--- src/main/scala/Ch20/CurrencyStudy/Currency.scala
+++ src/main/scala/Ch20/CurrencyStudy/Currency.scala
@@ -66,6 +66,18 @@ object Japan extends CurrencyZone {
 }
 
 
+object Converter {
+    var exchangeRate = Map(
+        "USD" -> Map("USD" -> 1.0   , "EUR" -> 0.7596,
+            "JPY" -> 1.211 , "CHF" -> 1.223),
+        "EUR" -> Map("USD" -> 1.316 , "EUR" -> 1.0   ,
+            "JPY" -> 1.594 , "CHF" -> 1.623),
+        "JPY" -> Map("USD" -> 0.8257, "EUR" -> 0.6272,
+            "JPY" -> 1.0   , "CHF" -> 1.018),
+        "CHF" -> Map("USD" -> 0.8108, "EUR" -> 0.6160,
+            "JPY" -> 0.982 , "CHF" -> 1.0  )
+    ) }
+
 // -------------------------------------------------------- below is my own attempt at this, above is from the book
 abstract class SCurrency {
     val amount: Long

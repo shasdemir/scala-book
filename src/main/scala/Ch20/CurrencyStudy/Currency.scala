0ae7ff655dbcd39424c516c6d33a2d021c3908b7
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-01-31 04:00:22 +0200

    My implementation works. The book says it can be better, though.

--- src/main/scala/Ch20/CurrencyStudy/Currency.scala
+++ src/main/scala/Ch20/CurrencyStudy/Currency.scala
@@ -5,6 +5,8 @@ package Ch20.CurrencyStudy
  */
 abstract class AbstractCurrency {
     type Currency <: AbstractCurrency
+    def make(amount: Long): Currency
+
     val amount: Long
     def designation: String
 
@@ -27,26 +29,31 @@ abstract class Euro extends AbstractCurrency {
 
 
 
-// ---------------------------------------------------------------------------------------
+// -------------------------------------------------------- below is my own attempt at this, above is from the book
 abstract class SCurrency {
     val amount: Long
     val threeLetterName: String
     type Designation <: SCurrency
+    def make(amount: Long): Designation
 
     override def toString = amount+ " " + threeLetterName
 
-    def + (that: Designation) = new Designation(amount + that.amount)
-    def * (x: Double) = new Designation((amount * x).toLong)
+    def + (that: Designation) = make(amount + that.amount)
+    def * (x: Double) = make((amount * x).toLong)
 }
 
 
 class EuroAmount(val amount: Long) extends SCurrency {
     val threeLetterName = "EUR"
     type Designation = EuroAmount
+
+    def make(amount: Long) = new EuroAmount(amount)
 }
 
 
 class USDAmount(val amount: Long) extends SCurrency {
     val threeLetterName = "USD"
     type Designation = USDAmount
+
+    def make(amount: Long) = new USDAmount(amount)
 }

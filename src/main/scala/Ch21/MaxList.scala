1dcb339b918a1ce11f1a5db4bee0f37bdd2da699
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-02-01 23:48:57 +0200

    Added maxListUpBound.

---
+++ src/main/scala/Ch21/MaxList.scala
@@ -0,0 +1,16 @@
+package Ch21
+
+/**
+ * Created by sukruhasdemir on 01/02/15.
+ */
+object MaxList {
+
+    def maxListUpBound[T <: Ordered[T]](elements: List[T]): T =
+        elements match {
+            case List() => throw new IllegalArgumentException("empty list!")
+            case List(x) => x
+            case first :: rest =>
+                val maxRest = maxListUpBound(rest)
+                if (first > maxRest) first else maxRest
+        }
+}

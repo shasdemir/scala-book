5afb91a910dda6c2c9abf65d1dd7590cb0d4ae58
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-02-01 23:39:18 +0200

    Added drink preference.

--- src/main/scala/Ch21/Greeter.scala
+++ src/main/scala/Ch21/Greeter.scala
@@ -5,11 +5,14 @@ package Ch21
  */
 
 class PreferredPrompt(val preference: String)
+class PreferredDrink(val preference: String)
 
 
 object Greeter {
-    def greet(name: String)(implicit prompt: PreferredPrompt): Unit = {
+    def greet(name: String)(implicit prompt: PreferredPrompt, drink: PreferredDrink): Unit = {
         println("Welcome, " + name + ". The system is ready.")
+        println("But while you work, ")
+        println("why not enjoy a cup of " + drink.preference + "?")
         println(prompt.preference)
     }
 }
@@ -17,4 +20,5 @@ object Greeter {
 
 object JoesPrefs {
     implicit val prompt = new PreferredPrompt("Yes, master> ")
+    implicit val drink = new PreferredDrink("tea")
 }

157336e73f72861503ba4d84e70f0f76ca1e5672
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-02-02 00:11:16 +0200

    Added maxListImpParam.

--- src/main/scala/Ch21/MaxList.scala
+++ src/main/scala/Ch21/MaxList.scala
@@ -5,12 +5,21 @@ package Ch21
  */
 object MaxList {
 
-    def maxListUpBound[T](elements: List[T])(implicit orderer: T => Ordered[T]): T =
+    def maxListUpBound[T <: Ordered[T]](elements: List[T]): T =
         elements match {
             case List() => throw new IllegalArgumentException("empty list!")
             case List(x) => x
             case first :: rest =>
                 val maxRest = maxListUpBound(rest)
                 if (first > maxRest) first else maxRest
+        }
+
+    def maxListImpParam[T](elements: List[T])(implicit orderer: T => Ordered[T]): T =
+        elements match {
+            case List() => throw new IllegalArgumentException("empty list!")
+            case List(x) => x
+            case first :: rest =>
+                val maxRest = maxListImpParam(rest)(orderer)
+                if (orderer(first) > maxRest) first else maxRest
         }
 }

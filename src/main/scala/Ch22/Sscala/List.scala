af8c0f4a0eb2468ff77b3de5257aa94ba7c6408f
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-03-02 13:34:57 +0200

    Added ::.

---
+++ src/main/scala/Ch22/Sscala/List.scala
@@ -0,0 +1,28 @@
+package Ch22.Sscala
+
+/**
+ * Created by sukruhasdemir on 01/03/15.
+ * This package contains an implementation of List class.
+ */
+
+abstract class List[+T] {
+    def isEmpty: Boolean
+    def head: T
+    def tail: List[T]
+}
+
+
+case object Nil extends List[Nothing] {
+    override def isEmpty = true
+
+    def head: Nothing = throw new NoSuchElementException("head of an empty list")
+
+    def tail: List[Nothing] = throw new NoSuchElementException("tail of an empty list")
+}
+
+
+final case class ::[T](hd: T, tl: List[T]) extends List[T] {
+    def head = hd
+    def tail = tl
+    override def isEmpty: Boolean = false
+}

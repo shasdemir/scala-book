84fbaa169f54ae2d715a2ffa2da558b64a501ed0
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-03-23 00:32:41 +0200

    My version of pairs

--- src/main/scala/Ch23/ForExprSandbox.scala
+++ src/main/scala/Ch23/ForExprSandbox.scala
@@ -15,4 +15,11 @@ object ForExprSandbox {
     val julie = Person("Julie", false, lara, bob)
 
     val persons = List(lara, bob, julie)
+
+    // find pairs of all mothers and their children - how I would do it w/o looking at the code in the book
+    def isMother(sim: Person): Boolean = !sim.isMale && sim.children.nonEmpty
+
+    def makeChildrenPairs(sim: Person) = sim.children.map((child: Person) => (sim.name, child.name))
+
+    val pairs = persons.filter(isMother).flatMap(makeChildrenPairs)
 }

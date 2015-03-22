a8738f0206b6e28b466f746ac71077f6a815c9ec
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-03-23 00:38:13 +0200

    Removed unnecessary bits.

--- src/main/scala/Ch23/ForExprSandbox.scala
+++ src/main/scala/Ch23/ForExprSandbox.scala
@@ -4,9 +4,6 @@ package Ch23
  * Created by sukruhasdemir on 22/03/15.
  */
 object ForExprSandbox {
-    def main(args: Array[String]): Unit = {
-        println("it works!")
-    }
 
     case class Person(name: String, isMale: Boolean, children: Person*)
 
@@ -19,7 +16,7 @@ object ForExprSandbox {
     // find pairs of all mothers and their children - how I would do it w/o looking at the code in the book
     def isMother(sim: Person): Boolean = !sim.isMale && sim.children.nonEmpty
 
-    def makeChildrenPairs(sim: Person) = sim.children.map((child: Person) => (sim.name, child.name))
+    def makeChildrenPairs(sim: Person) = sim.children.map(child => (sim.name, child.name))
 
     val pairs = persons.filter(isMother).flatMap(makeChildrenPairs)
 }

a619345cacdee46fc447f0b1832de1ade7e3bd4e
Şükrü Hasdemir <sukruhasdemir1987@gmail.com>
2015-04-08 05:12:41 +0300

    Start of work on n-queens.

---
+++ src/main/scala/Ch23/NQueens.scala
@@ -0,0 +1,12 @@
+package Ch23
+
+/**
+ * Created by sukruhasdemir on 08/04/15.
+ */
+class NQueens {
+    def main(args: List[String]): List[List[(Int, Int)]] = {
+        val boardSize = args.head.toInt
+
+        
+    }
+}
